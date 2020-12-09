df5abca32744a905466a2b3296cb08cb0bc12412
AndrasMarro <72513821+AndrasMarro@users.noreply.github.com>
2020-12-09 12:21:10 +0100

    Projektdateien hinzufügen.

---
+++ RegEx/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("daten.txt"))
+            {
+                string zeile;
+                while((zeile=reader.ReadLine()) != null)
+                {
+                    
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
